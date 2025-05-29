cf0893ca645983fc964d90fd74cdb1ec0714a3e6
Anna Naylor <143585907+annaina@users.noreply.github.com>
2025-05-30 01:03:17 +0200

    Update setup.py

--- ur_mover/setup.py
+++ ur_mover/setup.py
@@ -1,1 +1,28 @@
+from setuptools import setup
+import os
+from glob import glob
 
+package_name = 'ur_mover'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        # Inkluder launch-filer fra src/launch/
+        (os.path.join('share', package_name, 'launch'), glob('src/launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jørgen N. Melling',
+    maintainer_email='din@email.com',
+    description='Node som sender posisjoner til UR-robot og gir status',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ur_mover_node = ur_mover.ur_mover_node:main',
+        ],
+    },
+)
