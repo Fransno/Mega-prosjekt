cceb0f215d6731b24154ec5af752f8467ddf1569
Anna Naylor <143585907+annaina@users.noreply.github.com>
2025-05-29 17:31:56 +0200

    Update movement_planner_node.py

--- movement_planner/movement_planner/movement_planner_node.py
+++ movement_planner/movement_planner/movement_planner_node.py
@@ -3,6 +3,7 @@ import json
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
 
+# Last inn konfigurasjonsfil
 with open('config.json') as file:
     data = json.load(file)
 
@@ -11,6 +12,7 @@ class MovementPlannerNode(Node):
     def __init__(self):
         super().__init__('movement_planner')
 
+        # Kart for å oversette fargekoder til navn
         self.color_names = {
             1.0: "red",
             2.0: "yellow",
@@ -18,18 +20,22 @@ class MovementPlannerNode(Node):
             4.0: "green"
         }
 
+        # Ønsket rekkefølge på kuber, hentet fra config.json 
         self.cube_order = [data["cube1"], data["cube2"], data["cube3"]]
-        self.z = data["z"]
-        self.debug_mode = data["debug_mode"]
+        self.z = data["z"] # Z-høyde å bruke for alle posisjoner
+        self.debug_mode = data["debug_mode"] # Debug flagg
 
+        # ROS-abonnement og publisher
         self.detected_cubes_sub = self.create_subscription(Float64MultiArray,'detected_cubes',self.detected_cubes_callback,10)
         self.planned_pos_pub = self.create_publisher(Float64MultiArray,'planned_pos',10)
 
-
+    # Behandler innkommende kubeposisjoner fra fargegjenkjenning
     def detected_cubes_callback(self, msg):
         data = msg.data
         i = 0
         cube_positions = {}
+
+        # Parse hver kube: (fargekode, x, y)
         while i < len(data):
             color_code = data[i]
             x = data[i+1]
@@ -38,12 +44,12 @@ class MovementPlannerNode(Node):
             cube_positions[color_name] = (x, y)
             i += 3
 
-        # Publish only if ALL desired cubes are present
+        # Sjekk at ALLE ønskede kuber er funnet før publisering
         if all(color in cube_positions for color in self.cube_order):
             positions = []
             for color in self.cube_order:
                 x, y = cube_positions[color]
-                positions.extend([x, y, self.z])
+                positions.extend([x, y, self.z]) # Legg til Z-høyde
             out_msg = Float64MultiArray()
             out_msg.data = positions
             self.planned_pos_pub.publish(out_msg)
