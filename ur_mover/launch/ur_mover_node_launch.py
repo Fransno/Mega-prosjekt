5a0ea59fd55be5d008993bc35207ad9014c8e777
Anna Naylor <143585907+annaina@users.noreply.github.com>
2025-05-30 01:06:04 +0200

    Update ur_mover_node_launch.py

--- ur_mover/launch/ur_mover_node_launch.py
+++ ur_mover/launch/ur_mover_node_launch.py
@@ -1,1 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ur_mover',  
+            executable='ur_mover_node',  
+            name='ur_mover_node',
+            output='screen'
+        )
+    ])
 
