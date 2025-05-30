d5b58d6d8d54685ebadb42a42212d281984f4006
Anna Naylor <143585907+annaina@users.noreply.github.com>
2025-05-30 03:24:23 +0200

    Update setup.py

--- error_handler/setup.py
+++ error_handler/setup.py
@@ -1,27 +1,37 @@
 from setuptools import find_packages, setup
 
-package_name = 'error_handler'
+package_name = 'error_handler'  # Navnet på ROS2-pakken
 
 setup(
     name=package_name,
     version='0.0.0',
+    
+    # Inkluderer alle Python-moduler i pakken, unntatt test/
     packages=find_packages(exclude=['test']),
+    
+    # Filer som skal installeres i riktig ROS2-sti
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/error_handler_launch.py']),
+            ['resource/' + package_name]),  # ROS2 krever en ressursfil
+        ('share/' + package_name, ['package.xml']),  # Pakke-metadata
+        ('share/' + package_name + '/launch', ['launch/error_handler_launch.py']),  # Launch-fil
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
+    
+    install_requires=['setuptools'],  # Python-avhengighet
+    zip_safe=True,  # OK for ROS2 (ikke behov for pakkeutpakking)
+    
     maintainer='frans',
     maintainer_email='TeemoTeemo123@outlook.com',
-    description='TODO: Package description',
+    description='TODO: Package description',  # Husk å fylle inn før levering
     license='Apache-2.0',
-    tests_require=['pytest'],
+    
+    tests_require=['pytest'],  # Testing
+
     entry_points={
         'console_scripts': [
+            # ROS2-node entry point – kobler ROS-kommando til Python-funksjon
             'error_handler_node = error_handler.error_handler_node:main',
         ],
     },
 )
+
